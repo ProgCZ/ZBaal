4c9bf6d6778a43de6621a34e9930ee13a2062c11
ProgCZ <zhaozengzhi1995@163.com>
2023-01-01 17:46:30 +0800

    feat: introduce Electron

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: path.resolve(__dirname, './dist/'),
+  server: {
+    port: 5173
+  },
   plugins: [vue()],
 })
